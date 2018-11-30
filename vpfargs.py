200c8e9765eef5cbf1a59394806b25ae77d2dcd3
Vykstorm <victorruizgomezdev@gmail.com>
2018-11-30 13:40:40 +0100

    Changed main module imports

--- vpfargs.py
+++ vpfargs.py
@@ -1,10 +1,5 @@
 
-
 # This module its exposed to the outside of this library
 
-# Include all decorators
-
-# Also expose all avaliable predefined validators and parsers
-from src.validators import *
-from src.parsers import *
 from src.decorators import validate, parse
+from src import parsers, validators
