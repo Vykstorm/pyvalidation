df5f4fa3bca47a2abc593ef1c09a6e3ba8d85975
Vykstorm <victorruizgomezdev@gmail.com>
2018-12-02 12:26:46 +0100

    Added ellipsis syntax

--- src/decorators.py
+++ src/decorators.py
@@ -3,7 +3,7 @@ This module defines the classes Decorator, ValidateInputDecorator and ParseInput
 validate is an alias of ValidateInputDecorator class, and parse is an alias of ParseInputDecorator
 '''
 
-from inspect import signature
+from inspect import signature, Parameter
 from .validators import Validator
 from .processors import ValidateInput, ParseInput
 from .wrappers import FuncWrapper
@@ -29,17 +29,61 @@ class Decorator:
         :param f: The function to decorate (Must be a callable object)
         :return: Returns a function wrapper (instance of the class Wrapper)
         '''
+
+        class Placeholder:
+            pass
+
+        def parse_ellipsis(args, s):
+            if Ellipsis not in args:
+                return args
+            k = args.index(Ellipsis)
+
+            a, b = list(args[:k]), list(args[k+1:])
+            if len(b) == 0:
+                return a
+            n = len(args) - 1
+
+            params = [param for param in s.parameters.values() if param.kind == Parameter.POSITIONAL_OR_KEYWORD]
+            m = len(params)
+            if n > m:
+                return args
+
+            c = [Placeholder] * (m - n)
+            return a + c + b
+
+        def merge_args_kwargs(A, B):
+            merged = []
+            for a, b in zip(A, B):
+                if (a == Placeholder) ^ (b == Placeholder):
+                    merged.append(a if a != Placeholder else b)
+                elif a == Placeholder and b == Placeholder:
+                    merged.append(Placeholder)
+                else:
+                    raise Exception()
+            return merged
+
+
         if not callable(f):
             raise TypeError()
 
         args, kwargs = self.args, self.kwargs
 
         s = signature(f, follow_wrapped=True)
-        s = s.replace(parameters=[param.replace(default=self.empty_arg) for param in s.parameters.values()])
-        bounded_args = s.bind(*args, **kwargs)
+
+        args = parse_ellipsis(args, s)
+        s = s.replace(parameters=[param.replace(default=Placeholder) for param in s.parameters.values()])
+        bounded_args = s.bind(*args)
         bounded_args.apply_defaults()
 
-        processor = self.create_processor(*bounded_args.args)
+        bounded_kwargs = s.bind(**kwargs)
+        bounded_kwargs.apply_defaults()
+
+        try:
+            args = merge_args_kwargs(bounded_args.args, bounded_kwargs.args)
+        except:
+            s.bind(*args, **kwargs)
+        args = [arg if arg != Placeholder else self.empty_arg for arg in args]
+        processor = self.create_processor(*args)
         wrapper = FuncWrapper(f) if not isinstance(f, FuncWrapper) else f
         wrapper.append(processor)
         return wrapper
