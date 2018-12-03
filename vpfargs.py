ea5f76669e7099e65608e668dc93d7403460672f
Vykstorm <victorruizgomezdev@gmail.com>
2018-12-03 13:28:30 +0100

    Changed import statements

--- vpfargs.py
+++ vpfargs.py
@@ -2,4 +2,5 @@
 # This module its exposed to the outside of this library
 
 from src.decorators import validate, parse
-from src import parsers, validators
+from src.validators import *
+from src.parsers import *
